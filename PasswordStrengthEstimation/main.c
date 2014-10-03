76e9d7ef9dcf257fad8d08bc5297972f24f885ec
MMY <kidd0717@mitake.com.tw>
2014-10-03 11:03:47 +0800

    Initial Commit

---
+++ PasswordStrengthEstimation/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PasswordStrengthEstimation
+//
+//  Created by MITAKE on 2014/10/3.
+//  Copyright (c) 2014年 mitake. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
